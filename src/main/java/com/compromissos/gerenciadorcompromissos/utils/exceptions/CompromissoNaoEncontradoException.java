eb3b33aad4b2626e8479ce0c45f89574e5797327
RaphaelaCristina <raphitasp@msn.com>
2025-03-31 19:55:46 -0300

    feat: exceções

---
+++ src/main/java/com/compromissos/gerenciadorcompromissos/utils/exceptions/CompromissoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.compromissos.gerenciadorcompromissos.utils.exceptions;
+
+public class CompromissoNaoEncontradoException extends RuntimeException {
+
+    public CompromissoNaoEncontradoException(String compromissoId) {
+        super("Compromisso de ID:  " + compromissoId + " não localizado.");
+    }
+}
