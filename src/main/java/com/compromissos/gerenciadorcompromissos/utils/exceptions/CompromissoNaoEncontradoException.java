6e38a6206bcde875c124b3ad8f1517e0619f1207
RaphaelaCristina <raphitasp@msn.com>
2025-04-06 15:24:54 -0300

    feat: ajuste exception

--- src/main/java/com/compromissos/gerenciadorcompromissos/utils/exceptions/CompromissoNaoEncontradoException.java
+++ src/main/java/com/compromissos/gerenciadorcompromissos/utils/exceptions/CompromissoNaoEncontradoException.java
@@ -3,6 +3,6 @@ package com.compromissos.gerenciadorcompromissos.utils.exceptions;
 public class CompromissoNaoEncontradoException extends RuntimeException {
 
     public CompromissoNaoEncontradoException(String compromissoId) {
-        super("Compromisso de  ID:  " + compromissoId + " não localizado.");
+        super("Compromisso de ID: " + compromissoId + " não localizado.");
     }
 }
