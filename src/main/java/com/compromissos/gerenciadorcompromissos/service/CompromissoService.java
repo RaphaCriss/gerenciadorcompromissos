364d65c16b6ec110b5a6906ede4088d4a27accc2
RaphaelaCristina <raphitasp@msn.com>
2025-06-15 22:22:59 -0300

    feat: refatorando CompromissoService

--- src/main/java/com/compromissos/gerenciadorcompromissos/service/CompromissoService.java
+++ src/main/java/com/compromissos/gerenciadorcompromissos/service/CompromissoService.java
@@ -3,49 +3,33 @@ package com.compromissos.gerenciadorcompromissos.service;
 import com.compromissos.gerenciadorcompromissos.controller.CreateCompromissoDto;
 import com.compromissos.gerenciadorcompromissos.entity.CompromissoEntity;
 import com.compromissos.gerenciadorcompromissos.repository.CompromissoRepository;
-import com.compromissos.gerenciadorcompromissos.utils.exceptions.CompromissoJaExistenteException;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
-
 
 @Service
 public class CompromissoService {
 
-    private CompromissoRepository compromissoRepository;
+    private final CompromissoRepository compromissoRepository;
 
-    public CompromissoService(CompromissoRepository compromissoRepository) {
-        this.compromissoRepository = compromissoRepository;
+    public CompromissoService() {
+        compromissoRepository = null;
     }
-
 
     public CompromissoEntity createCompromisso(CreateCompromissoDto createCompromissoDto) {
 
-        //DTO -> ENTITY
-        var entity = new CompromissoEntity(
-                UUID.randomUUID(),
-                createCompromissoDto.data(),
-                createCompromissoDto.hora(),
-                createCompromissoDto.descricao(),
-                createCompromissoDto.local(),
-                Instant.now(),
-                null
-        );
-
-        if (compromissoRepository.existsById(entity.getCompromissoId())) {
-
-            throw new CompromissoJaExistenteException(entity.getCompromissoId());
-        }
+        var entity = CompromissoEntity.builder()
+                .data(createCompromissoDto.data())
+                .hora(createCompromissoDto.hora())
+                .descricao(createCompromissoDto.descricao())
+                .local(createCompromissoDto.local())
+                .build();
 
         return compromissoRepository.save(entity);
-
     }
 
-    public Optional<CompromissoEntity> getCompromissoById(String compromissoId){
-
+    public Optional<CompromissoEntity> getCompromissoById(String compromissoId) {
         return compromissoRepository.findById(UUID.fromString(compromissoId));
     }
-
 }
