82730d03cbff9838890e95cefd7c475fc6d9d85d
RaphaelaCristina <raphitasp@msn.com>
2025-07-09 21:34:13 -0300

    feat: Refatorando métodos e adicionando campo idTelegram e refatorando os campos de data e hora

--- src/main/java/com/compromissos/gerenciadorcompromissos/repository/CompromissoRepository.java
+++ src/main/java/com/compromissos/gerenciadorcompromissos/repository/CompromissoRepository.java
@@ -3,15 +3,19 @@ package com.compromissos.gerenciadorcompromissos.repository;
 import com.compromissos.gerenciadorcompromissos.entity.CompromissoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import java.time.LocalDate;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CompromissoRepository extends JpaRepository<CompromissoEntity, UUID> {
 
+    List<CompromissoEntity> findByIdTelegram(Long idTelegram);
 
-    List<CompromissoEntity> findByData(LocalDate data);
+    List<CompromissoEntity> findByDataHoraAndIdTelegram(LocalDateTime dataHora, Long idTelegram);
 
-    @Query("SELECT c FROM CompromissoEntity c WHERE MONTH(c.data) = :mes AND YEAR(c.data) = :ano")
-    List<CompromissoEntity> findByMesEAno(int mes, int ano);
+    Optional<CompromissoEntity> findByCompromissoIdAndIdTelegram(UUID compromissoId, Long idTelegram);
+
 }
