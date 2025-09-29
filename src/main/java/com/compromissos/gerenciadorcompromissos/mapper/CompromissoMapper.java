ebc01af364bd5c8636f1780887af52fcb5994218
RaphaCriss <raphitasp@msn.com>
2025-09-28 21:38:05 -0300

    feat: refatorando mapper para nova aplicação

--- src/main/java/com/compromissos/gerenciadorcompromissos/mapper/CompromissoMapper.java
+++ src/main/java/com/compromissos/gerenciadorcompromissos/mapper/CompromissoMapper.java
@@ -1,14 +1,24 @@
 package com.compromissos.gerenciadorcompromissos.mapper;
 
-import com.compromissos.gerenciadorcompromissos.controller.CompromissoDto;
+import com.compromissos.gerenciadorcompromissos.dto.CompromissoDto;
+import com.compromissos.gerenciadorcompromissos.dto.CompromissoResponseDto;
+import com.compromissos.gerenciadorcompromissos.dto.CompromissoUpdateDto;
 import com.compromissos.gerenciadorcompromissos.entity.CompromissoEntity;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface CompromissoMapper {
 
-    CompromissoMapper INSTANCE = Mappers.getMapper(CompromissoMapper.class);
 
     CompromissoEntity toEntity(CompromissoDto dto);
+
+    CompromissoResponseDto toResponseDto(CompromissoEntity entity);
+
+    // Atualização parcial: MapStruct permite isso com @BeanMapping e ignoreByDefault
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromDto(CompromissoUpdateDto dto, @MappingTarget CompromissoEntity entity);
 }
