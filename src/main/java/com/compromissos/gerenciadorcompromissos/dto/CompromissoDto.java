a4c249219330e0bbcd92174e5102188ed5431376
RaphaCriss <raphitasp@msn.com>
2025-09-28 21:19:04 -0300

    feat: ajuste CompromissoDto

--- src/main/java/com/compromissos/gerenciadorcompromissos/dto/CompromissoDto.java
+++ src/main/java/com/compromissos/gerenciadorcompromissos/dto/CompromissoDto.java
@@ -6,13 +6,13 @@ import jakarta.validation.constraints.Pattern;
 
 import java.time.LocalDateTime;
 
-public record CompromissoDto(
+public record CompromissoDto( //Criação
         @NotBlank String titulo, //@NotBlank é usada para validar que um campo String não está nulo, vazio ou apenas com espaços em branco.
         String descricao,
         LocalDateTime dataHora,
         @Pattern(regexp = "\\d{5}-?\\d{3}") String cep, //Exatamente 5 dígitos (números), Um hífen opcional (- pode ou não existir), Exatamente 3 dígitos
         @NotBlank String cidade, //@NotBlank é usada para validar que um campo String não está nulo, vazio ou apenas com espaços em branco.
-        @NotBlank String estaddo, //@NotBlank é usada para validar que um campo String não está nulo, vazio ou apenas com espaços em branco.
+        @NotBlank String estado, //@NotBlank é usada para validar que um campo String não está nulo, vazio ou apenas com espaços em branco.
         @Min(2) Integer alertaDiasAntes, //Mínimo 2 dias
         Long idTelegram
 ){}
